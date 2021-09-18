55a70bdcf52e41bb02d6a3f3bc4c7dd6dc47ffc0
alsxs <oh@hotbox.ru>
2021-09-19 02:46:43 +0300

    for 15.1

--- clokub/15.1/p3-4/ec2s.tf
+++ clokub/15.1/p3-4/ec2s.tf
@@ -71,6 +71,13 @@ resource "aws_security_group" "main" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_key_pair" "aws_pk" {
